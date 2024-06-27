6eec4b7bb9431f57c465347da8cd725b1a8a5d9e
qyhuang <719155174@qq.com>
2024-06-27 07:45:17 +0000

    update: multi nodes train

--- src/train.py
+++ src/train.py
@@ -16,6 +16,14 @@ from llamafactory.train.tuner import run_exp
 import wandb
 import os
 
+os.environ["NCCL_DEBUG"] = "WARN"  # INFO 或 "WARN" 或 "ERROR" 以减少输出量
+os.environ["NCCL_SOCKET_IFNAME"] = "ens10f0np0"  # 替换为你的网络接口名
+# 添加以下变量速度非常慢
+# os.environ["NCCL_IB_DISABLE"] = "1"  # 禁用IB网络，如果不使用IB网络的话
+# os.environ["NCCL_NET_GDR_LEVEL"] = "5"
+
+os.environ["NCCL_P2P_LEVEL"] = "NVL" #NVLINK
+
 # wandb 登录
 # api_key = "60a763e8c62e44eff794afc94793f43732caa063"
 api_key = "904d9b7af10d7a2ea3f3d1be89703a53c20deb47"
--- train_bash/train_sft_multi.sh
+++ train_bash/train_sft_multi.sh
@@ -1,13 +1,21 @@
-deepspeed --include localhost:0,1,2,3,4,5,6,7 --master_port=29500 src/train.py \
+MASTER_ADDR=10.205.23.131
+MASTER_PORT=29500
+
+NODE_RANK=0
+NUM_GPUS_PER_NODE=8
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+torchrun --nproc_per_node=$NUM_GPUS_PER_NODE --nnodes=2 --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT src/train.py \
     --stage sft \
     --model_name_or_path /data0/pretrained-models/Qwen2-7B-Instruct \
     --template qwen \
     --do_train \
-    --dataset small_data \
+    --dataset all_data \
     --finetuning_type full \
-    --output_dir /data4/sft_output/qwen2-instruct \
+    --output_dir /data4/sft_output/qwen2-instruct131 \
     --overwrite_cache \
-    --cache_dir /data4/sft_output/.cache_dir \
+    --cache_dir /data4/sft_output/.cache_dir131 \
     --overwrite_output_dir \
     --per_device_train_batch_size 3 \
     --per_device_eval_batch_size 3 \
@@ -19,16 +27,18 @@ deepspeed --include localhost:0,1,2,3,4,5,6,7 --master_port=29500 src/train.py \
     --optim_args 'min_lr_ratio=0.1' \
     --lr_scheduler_type cosine \
     --warmup_ratio 0.01 \
-    --logging_steps 2 \
+    --logging_steps 20 \
     --save_strategy "steps" \
-    --save_steps 20 \
+    --save_steps 500 \
     --eval_strategy "steps" \
-    --val_size 0.05 \
-    --eval_steps 20 \
+    --val_size 0.01 \
+    --eval_steps 500 \
     --cutoff_len 8192 \
     --num_train_epochs 2 \
     --plot_loss \
     --bf16 true \
-    --deepspeed examples/deepspeed/ds_z2_offload_config.json \
+    --deepspeed examples/deepspeed/ds_z2_config.json \
     --report_to "wandb" \
-    --flash_attn fa2 
+    --flash_attn fa2 \
+    --ddp_timeout 3600 \
+    &> logs/training.log
